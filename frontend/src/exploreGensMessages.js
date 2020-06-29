3d4c5b040083123e5e0872b98a4e59303e6510bb
Paola Laufer <paola.laufer@sap.com>
2020-06-29 16:11:47 +0300

    Update exploreGensMessages.js
    Reviewed.
    Are you sure you want the legal note to be in all caps?

--- frontend/src/exploreGensMessages.js
+++ frontend/src/exploreGensMessages.js
@@ -1,10 +1,10 @@
 export default {
     title: "Explore Generators",
     description: "This view enables the exploration and installation of external open source Yeoman generators.",
-    legal_note: "NOTE: IN NO EVENT WILL SAP BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, PUNITIVE, SPECIAL ORCONSEQUENTIAL DAMAGES RELATED TO ANY USE OF EXTERNAL GENERATORS EXPLORED AND INSTALLED.",
+    legal_note: "NOTE: IN NO EVENT WILL SAP BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, PUNITIVE, SPECIAL OR CONSEQUENTIAL DAMAGES RELATED TO ANY USE OF EXTERNAL GENERATORS EXPLORED AND/OR INSTALLED.",
     search: "Search for Generators",
     recommended: "Recommended",
-    refine_search: "You may need to refine your search",
+    refine_search: "Refine your search",
     results_out_of_total: (gensQuantity, totalGensQuantity) => `Showing ${gensQuantity} out of ${totalGensQuantity} results.`,
     results: (totalGensQuantity) => `Showing ${totalGensQuantity} results.`
 }; 
